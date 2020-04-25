99c2b454ebc770e0ed927f9304e9770d451258f2
NelsonC80 <62479115+NelsonC80@users.noreply.github.com>
2020-04-25 23:46:22 +0100

    Add files via upload

--- TP/Program.cs
+++ TP/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="IPCA">
+// Copyright (c) IPCA. All rights reserved.</copyright>
+//-----------------------------------------------------------------------
+// <author>Emanuel Gomes</author>
+// <author>Nelson Cunha</author>
+// <desc> Hostel System Management - Basics in C#</desc>
+// <Date> 25 / 4 / 2020 </Date>
+// <version>1.0</version>
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
